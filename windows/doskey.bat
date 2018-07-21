62049ccac5184831f5680bf03e38aa23b99eb690
Dabson <cyphlix@gmail.com>
2018-07-21 10:58:34 +1000

    yeah baby super workflow

---
+++ windows/doskey.bat
@@ -0,0 +1,10 @@
+@Echo off
+rem HKEY_CURRENT_USER\Software\Microsoft\Command Processor AutoRun=C:\doskey.bat
+rem DO NOT USE THIS TO CHANGE THE DEFAULT DIRECTORY. It will bite you in the ass.
+
+doskey subl="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
+doskey sublime="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
+doskey gitdir=F: $T cd f:\git
+doskey g = git g $*
+
+@Echo on
