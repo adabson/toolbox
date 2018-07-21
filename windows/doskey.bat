896342fd97205b072f3dcf5c8ed56994cf08aee9
Dabson <cyphlix@gmail.com>
2018-07-21 11:03:07 +1000

    vince mcMahon.gif

--- windows/doskey.bat
+++ windows/doskey.bat
@@ -4,7 +4,7 @@ rem DO NOT USE THIS TO CHANGE THE DEFAULT DIRECTORY. It will bite you in the ass
 
 doskey subl="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
 doskey sublime="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
-doskey gitdir=F: $T cd f:\git
+doskey gitdir=F: $T cd f:\git\$*
 doskey g = git g $*
 
 @Echo on
