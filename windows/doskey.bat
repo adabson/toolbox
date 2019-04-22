f7f7b8e51c6fae7eedfc17667a8c7e6e9667e2e4
Dabson <cyphlix@gmail.com>
2019-04-22 11:03:32 +1000

    add xdir

--- windows/doskey.bat
+++ windows/doskey.bat
@@ -4,7 +4,8 @@ rem DO NOT USE THIS TO CHANGE THE DEFAULT DIRECTORY. It will bite you in the ass
 
 doskey subl="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
 doskey sublime="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
-doskey gitdir=F: $T cd f:\git\$*
+doskey gitdir=cd c:\git\$*
+doskey xdir=cd c:\git\wetbox\dabson.co\experiment\$*
 doskey g = git g $*
 
 @Echo on
