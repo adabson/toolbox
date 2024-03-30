a6556ecf6fdef0a848c58c9e7e98f0ba61ee10c1
Dabson <cyphlix@gmail.com>
2024-03-30 21:03:13 +1100

    er

---
+++ elden-ring/copyscript/boom.bat
@@ -0,0 +1,3 @@
+taskkill /IM eldenring.exe /F
+xcopy /s /y C:\Users\adabs\Desktop\copyscript\from\ER0000.sl2 C:\Users\adabs\AppData\Roaming\EldenRing\76561197986678099\ER0000.sl2
+start steam://rungameid/1245620
---
+++ elden-ring/copyscript/new-save.bat
@@ -0,0 +1,2 @@
+
+xcopy /s /y C:\Users\adabs\AppData\Roaming\EldenRing\76561197986678099\ER0000.sl2 C:\Users\adabs\Desktop\copyscript\from\ER0000.sl2
